ff0434757d8ac68ad659029c1af428fa164cd381
LuisBrodely <luisbrodely2@gmail.com>
2024-06-14 01:04:23 -0600

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,109 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { useState, useEffect } from "react";
+import { Platform, Text, View, StyleSheet, Alert } from "react-native";
+import * as Device from "expo-device";
+import * as Location from "expo-location";
+import MapView, { Marker } from "react-native-maps";
+import constants from "expo-constants";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+type LatLng = {
+  latitude: number;
+  longitude: number;
+};
+
+type Region = LatLng & {
+  latitudeDelta: number;
+  longitudeDelta: number;
+};
+
+interface LocationType {
+  location: Region;
+  locationFix: any;
+  mocked: boolean | undefined;
+}
 
 export default function HomeScreen() {
+  const [state, setState] = useState<LocationType>({
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.005,
+      longitudeDelta: 0.005,
+    },
+    locationFix: null,
+    mocked: true,
+  });
+
+  const getLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      console.log("Permission to access location was denied");
+      return;
+    }
+    await Location.hasServicesEnabledAsync();
+    await Location.watchPositionAsync(
+      {
+        accuracy: Location.Accuracy.Highest,
+        timeInterval: 1,
+      },
+      async (point: any) => {
+        const location = point.coords;
+        const { mocked } = await Location.getCurrentPositionAsync({
+          accuracy: Location.Accuracy.Highest,
+          timeInterval: 1,
+        });
+        setState({
+          location: {
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          },
+          locationFix: location,
+          mocked: mocked,
+        });
+      }
+    ).catch((error: any) => {
+      console.log(error);
+      return;
+    });
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>{state.mocked ? "Mocked" : "Real"}</Text>
+      <MapView style={styles.map}>
+        <Marker
+          coordinate={{
+            latitude: state.location.latitude,
+            longitude: state.location.longitude,
+          }}
+          title="You are here"
+          description="This is your current location"
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      </MapView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+    marginTop: constants.statusBarHeight,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  paragraph: {
+    fontSize: 18,
+    textAlign: "center",
+    marginBottom: 10,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
